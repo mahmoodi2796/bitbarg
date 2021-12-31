6b39b627d2ddc64b5013708c646d409b462db5d9
mahmoodi2796 <you@example.com>
2022-01-01 01:10:11 +0330

    pushing

--- src/components/Wage/Wage.js
+++ src/components/Wage/Wage.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import './Wage.css'
-
+import Headericon from '../Home/Headericon.js'
 export default function Wage() {
     return (
         <div className="container-wage">
-            
+            <Headericon/>
+            <div className="title-blue-header"><h1 className="title-blue-header-text">کارمزد ارزهای دیجیتال</h1></div>
+
+
         </div>
     )
 }
