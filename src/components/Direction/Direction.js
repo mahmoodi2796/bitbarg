97cadfbbd67d9351384204cc3954f4cfb66d74e8
mahdi mahmoodi <samymosvi@gmail.com>
2022-01-09 18:02:41 +0330

    pushing

--- src/components/Direction/Direction.js
+++ src/components/Direction/Direction.js
@@ -9,7 +9,276 @@ export default function Direction() {
       <div className="title-blue-header">
         <h1 className="title-blue-header-text-2">مسیر بیت برگ</h1>
       </div>
-      <div className="container-main-Direction"></div>
+      <div className="parent-container-main-Direction">
+        <div className="container-main-Direction">
+          <div className="border-page-Direction"></div>
+          <div className="container-circle">
+            <div className="circel">
+              خرداد <br />
+              ۱۴۰۰
+            </div>
+            <div className="circel-2">
+              خرداد <br />
+              ۱۴۰۰
+            </div>
+            <div className="circel-2 t3">
+              خرداد <br />
+              ۱۳۹۹
+            </div>{" "}
+            <div className="circel-3">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-4">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-5">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-6">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-7">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-8">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-9">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-10">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-11">
+              خرداد <br />
+              ۱۳۹۸
+            </div>{" "}
+            <div className="circel-12">
+              خرداد <br />
+              ۱۳۹۷
+            </div>{" "}
+            <div className="circel-13">
+              خرداد <br />
+              ۱۳۹۷
+            </div>{" "}
+            <div className="circel-14">
+              خرداد <br />
+              ۱۳۹۷
+            </div>{" "}
+            <div className="circel-15">
+              خرداد <br />
+              ۱۳۹۷
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading">
+              <h4 className="timeline-title">نسخه ۲.۴</h4>
+            </div>
+            <div className="timeline-body">
+              افزایش تعداد ارز ها به۲۱۵ عدد❖
+              <br />
+              عدم نیاز به ثبت شناسه تراکنش❖
+              <br />
+              تسهیل فرایند احراز هویت❖
+              <br />
+              راه اندازی سامانه تیکتینگ❖
+              <br />
+              رفع باگ های گزارش شده❖
+              <br />
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-2">
+              <h4 className="timeline-title">نسخه ۲.۴</h4>
+            </div>
+            <div className="timeline-body">
+              تغییر رابط کاربری❖
+              <br />
+              افزودن ۲۵ ارز جدید❖
+              <br />
+              امکان جستجوی ارزها❖
+              <br />
+              برچسب گذاری کیف پول ها❖
+              <br />
+              افزودن بیت گپ❖
+              <br />
+              افزودن سرویس اطلاع رسانی❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-3">
+              <h4 className="timeline-title">نسخه ۲.۳</h4>
+            </div>
+            <div className="timeline-body-2">
+              افزودن ۱۰ ارز دیجیتال❖
+              <br />
+              راه اندازی سیستم امور مشتریان❖
+              <br />
+              راه اندازی سیستم اطلاع رسانی❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-4">
+              <h4 className="timeline-title">نسخه ۲.۲</h4>
+            </div>
+            <div className="timeline-body-2">
+              ارائه نمودار در قیمت لحظه ای❖
+              <br />
+              ۲۴ ساعته شدن پشتیبانی کاربران❖
+              <br />
+              رفع باگ های گزارش شده❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-5">
+              <h4 className="timeline-title">نسخه ۲.۱</h4>
+            </div>
+            <div className="timeline-body-2">
+              بهبود فرایند فروش ارز❖
+              <br />
+              تسویه حساب آنی بانکهای سپه و آینده❖
+              <br />
+              رفع باگ های گزارش شده❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-2">
+              <h4 className="timeline-title">نسخه ۲.۰</h4>
+            </div>
+            <div className="timeline-body-2">
+              رفع باگ های گزارش شده❖
+              <br />
+              بهینه سازی فرایند خرید و فروش❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-3">
+              <h4 className="timeline-title">نسخه ۱.۹</h4>
+            </div>
+            <div className="timeline-body-2">
+              افزودن ارزهای آنکر،‌ داسک، وینک❖
+              <br />
+              افزودن ارزهای کوکز،‌ متال، پاندی❖
+              <br />
+              تسویه حساب آنی سفارشات خرید و فروش❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-4">
+              <h4 className="timeline-title">نسخه ۱.۸</h4>
+            </div>
+            <div className="timeline-body-2">
+              افزودن ارزهای دوج کوین و الراند❖
+              <br />
+              امکان تغییر آدرس کیف پول کاربر❖
+              <br />
+              اضافه شدن بلاگ در بیت برگ❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-5">
+              <h4 className="timeline-title">نسخه ۱.۷</h4>
+            </div>
+            <div className="timeline-body-2">
+              طراحی مجدد صفحه قیمت لحظه ای❖
+              <br />
+              ارتقاء روش استعلام مالکیت سیم‌کارت❖
+              <br />
+              افزودن استعلام ثبت احوال❖
+              <br />
+              اعمال شرط ۳کارت برای خرید های بالا❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-2">
+              <h4 className="timeline-title">نسخه ۱.۶</h4>
+            </div>
+            <div className="timeline-body-2">
+              استعلام کارت بانکی و شماره شبا❖
+              <br />
+              ارتقاء سطح احراز هویت کاربران❖
+              <br />
+              قرارگرفتن کدرهگیری بانکی در سفارشات❖
+              <br />
+              برطرف شدن برخی باگ های گزارش شده❖
+              <br />
+              افزودن ۳ ارز به فهرست ارزهای بیت برگ❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-3">
+              <h4 className="timeline-title">نسخه ۱.۵</h4>
+            </div>
+            <div className="timeline-body-2">
+              افزودن ارزهای آنکر،‌ داسک، وینک❖
+              <br />
+              افزودن ارزهای کوکز،‌ متال، پاندی❖
+              <br />
+              تسویه حساب آنی سفارشات خرید و فروش❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-4">
+              <h4 className="timeline-title">نسخه ۱.۴</h4>
+            </div>
+            <div className="timeline-body-2">
+              طراحی محیطی جذاب و زیبا برای کاربران❖
+              <br />
+              صفحه ورود و ثبت نام ساده❖
+              <br />
+              امکان مشاهده ریز جزئیات سفارشات❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-5">
+              <h4 className="timeline-title">نسخه ۱.۳</h4>
+            </div>
+            <div className="timeline-body-2">
+              ارتقاء سرورهای بیت برگ❖
+              <br />
+              رفع برخی باگ های گزارش شده❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-2">
+              <h4 className="timeline-title">نسخه ۱.۲</h4>
+            </div>
+            <div className="timeline-body-2">
+              راه اندازی بخش مبدل قیمت❖
+              <br />
+              افزایش تعداد ارزها به ۱۵ ارز❖
+            </div>
+          </div>
+          <div className="timeline-panel">
+            <div className="timeline-heading-3">
+              <h4 className="timeline-title">نسخه ۱.۱</h4>
+            </div>
+            <div className="timeline-body-2">
+              راه‌اندازی بخش خرید و فروش فوری❖
+              <br />
+              اضافه شدن بخش اخبار❖
+              <br />
+              .اضافه شدن بخش تحلیل تکنیکال❖
+            </div>
+          </div>
+          <div className="timeline-panel-2">
+            <div className="timeline-heading-4">
+              <h4 className="timeline-title">تولد بیت برگ </h4>
+            </div>
+            <div className="timeline-body-2">
+              ❤راه اندازی سامانه خرید و فروش❖
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
